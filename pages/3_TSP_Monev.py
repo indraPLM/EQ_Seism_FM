0a6cbc6b5cddde9be6920c482155910d541010d5
indraPLM <112387467+indraPLM@users.noreply.github.com>
2025-02-24 14:44:48 +0700

    Update 3_TSP_Monev.py

--- pages/3_TSP_Monev.py
+++ pages/3_TSP_Monev.py
@@ -67,7 +67,7 @@ df['fixedLon'] = df.lon.apply(fix_longitude)
 df['sizemag']=1000*df['mag']
 
 st.markdown(""" ### Peta Lokasi Gempabumi berdasarkan Diseminasi RTSP """)
-st.map(df, latitude="fixedLat", longitude="fixedLon", size="sizemag")
+st.map(df, latitude="fixedLat", longitude="fixedLon")
 
 st.markdown(""" ### Tabel RTSP BMKG """)
 st.dataframe(df)
