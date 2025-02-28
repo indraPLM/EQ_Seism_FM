f1bb91a90aa2e4f882100aa307f28eef3896a690
indraPLM <112387467+indraPLM@users.noreply.github.com>
2025-02-28 14:42:25 +0700

    Update 3_TSP_Monev.py

--- pages/3_TSP_Monev.py
+++ pages/3_TSP_Monev.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 import pandas as pd
 import streamlit as st
-
+import numpy as np
 
 st.set_page_config(page_title='TSP Monitoring dan Evaluasi',  layout='wide', page_icon="🌍")
 #st.title('Seismisitas dan Statistik Kegempaan')
@@ -24,28 +24,12 @@ with layout3[0]:
 with layout3[-1]: 
     East = st.number_input('East:', 142.0)
 
-url='https://rtsp.bmkg.go.id/publicbull.php'
-page=requests.get(url)
-url_pages=BeautifulSoup(page.text, 'html')
-
-table=url_pages.find('table')
-rows=table.find_all("td",{"class":"txt11pxarialb"})
-tsp_table=[]
-for row in rows:
-    cells = row.find_all('div')
-    for cell in cells:
-        #data=cell.text
-        tsp_table.append(cell.text)
-
 def split_list(lst, chunk_size):
     chunks = []
     for i in range(0, len(lst), chunk_size):
         chunk = lst[i:i + chunk_size]
         chunks.append(chunk)
     return chunks
-
-chunk_size = 9
-chunks = split_list(tsp_table, chunk_size)
 
 def fix_latitude(x):
     if x.endswith('S'):
@@ -61,34 +45,66 @@ def fix_longitude(y):
         y = y.strip('E')
     return y
 
-date_time,mag,depth,lat,lon,typ,num_bull,evt_group=[],[],[],[],[],[],[],[]
-for lst in chunks:
-    a='%s %s' %(lst[0],lst[1])
-    date_time.append(a)
-    b=float(lst[2])
-    mag.append(b)
-    c=float(lst[3])
-    depth.append(c)
-    d=lst[4]
-    lat.append(d)
-    e=lst[5]
-    lon.append(e)
-    f=lst[6]
-    typ.append(f)
-    g=lst[7]
-    num_bull.append(g)
-    h=lst[8]
-    evt_group.append(h)
+def get_rtsp(url):
+    page=requests.get(url)
+    url_pages=BeautifulSoup(page.text, 'html')
 
-df=pd.DataFrame({'date_time':date_time,'mag':mag,'depth':depth,'lat':lat,'lon':lon,'typ':typ,'num_bull':num_bull,'evt_group':evt_group})
-df['date_time']=pd.to_datetime(df['date_time'])
-df['fixedLat'] = df.lat.apply(fix_latitude)
-df['fixedLat'] = pd.to_numeric(df['fixedLat'],errors = 'coerce')
+    table=url_pages.find('table')
+    rows=table.find_all("td",{"class":"txt11pxarialb"})
+    tsp_table=[]
+    for row in rows:
+        cells = row.find_all('div')
+        for cell in cells:
+            #data=cell.text
+            tsp_table.append(cell.text)
+    chunk_size = 9
+    chunks = split_list(tsp_table, chunk_size)
+    
+    date_time,mag,depth,lat,lon=[],[],[],[],[]
+    typ,num_bull,evt_group=[],[],[]
+    for lst in chunks:
+        a='%s %s' %(lst[0],lst[1])
+        date_time.append(a)
+        b=float(lst[2])
+        mag.append(b)
+        c=float(lst[3])
+        depth.append(c)
+        d=lst[4]
+        lat.append(d)
+        e=lst[5]
+        lon.append(e)
+        f=lst[6]
+        typ.append(f)
+        g=lst[7]
+        num_bull.append(g)
+        h=lst[8]
+        evt_group.append(h)
 
-df['fixedLon'] = df.lon.apply(fix_longitude)
-df['fixedLon'] = pd.to_numeric(df['fixedLon'],errors = 'coerce')
+    df=pd.DataFrame({'date_time':date_time,'mag':mag,'depth':depth,
+                     'lat':lat,'lon':lon,'typ':typ,'num_bull':num_bull,
+                     'evt_group':evt_group})
+    
+    df['date_time']=pd.to_datetime(df['date_time'])
+    df['fixedLat'] = df.lat.apply(fix_latitude)
+    df['fixedLat'] = pd.to_numeric(df['fixedLat'],errors = 'coerce')
 
-df['sizemag']=1000*df['mag']
+    df['fixedLon'] = df.lon.apply(fix_longitude)
+    df['fixedLon'] = pd.to_numeric(df['fixedLon'],errors = 'coerce')
+    df['sizemag']=1000*df['mag']
+    
+    return df
+# RTSP halaman awal
+df_awal=get_rtsp('https://rtsp.bmkg.go.id/publicbull.php')
+# RTSP halaman selanjutnya
+pages=14
+a=np.arange(2,pages+1,1)
+df=[]
+for i in a:
+    temp=get_rtsp('https://rtsp.bmkg.go.id/publicbull.php?halaman=%s' % (i))
+    df.append(temp)
+df_rtsp=pd.concat([df_awal,df[0],df[1],df[2],df[3],df[4],df[5],df[6],df[7],
+                  df[8],df[9],df[10],df[11],df[12]], ignore_index=True)
+print(df_rtsp)
 
 st.markdown(""" ### Peta Lokasi Gempabumi berdasarkan Diseminasi RTSP """)
 st.map(df, latitude="fixedLat", longitude="fixedLon", size="sizemag")
