6ef7f9ea8f9ebb263ac5cf246c0c725328945ce7
indraPLM <112387467+indraPLM@users.noreply.github.com>
2025-03-02 21:35:38 +0700

    Update 2_FM_Catalog.py

--- pages/2_FM_Catalog.py
+++ pages/2_FM_Catalog.py
@@ -179,3 +179,29 @@ cmt=df[['event_id','date_time','fixedLon','fixedLat','mag',
                   'depth','S1','D1','R1','S2','D2','R2']]
 
 st.dataframe(cmt)
+
+import matplotlib.pyplot as plt
+import cartopy.crs as ccrs
+
+from obspy.imaging.beachball import beach
+
+
+projection = ccrs.PlateCarree(central_longitude=142.0)
+
+fig = plt.figure(dpi=150)
+ax = fig.add_subplot(111, projection=projection)
+ax.set_extent((-180, 180, -90, 90))
+ax.coastlines()
+ax.gridlines()
+
+x, y = projection.transform_point(x=142.36929, y=38.3215,
+                                  src_crs=ccrs.Geodetic())
+focmecs = [0.136, -0.591, 0.455, -0.396, 0.046, -0.615]
+
+ax = plt.gca()
+b = beach(focmecs, xy=(x, y), width=10, linewidth=1, alpha=0.85)
+b.set_zorder(10)
+ax.add_collection(b)
+
+st.pyplot(fig)
+#plt.show()
