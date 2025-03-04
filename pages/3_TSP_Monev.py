ad3334fead3265288bfd281cd130e12ff2e438a8
indraPLM <112387467+indraPLM@users.noreply.github.com>
2025-03-04 07:50:22 +0700

    Update 3_TSP_Monev.py

--- pages/3_TSP_Monev.py
+++ pages/3_TSP_Monev.py
@@ -10,7 +10,7 @@ st.set_page_config(page_title='TSP Monitoring dan Evaluasi',  layout='wide', pag
 st.sidebar.header("Input Parameter :")
  
 date_start=st.sidebar.date_input('Start Time:', value="datetime.datetime")
-date_end=st.sidebar.date_input('End Time:', value="datetime.datetime"))
+date_end=st.sidebar.date_input('End Time:', value="datetime.datetime")
 
 def split_list(lst, chunk_size):
     chunks = []
