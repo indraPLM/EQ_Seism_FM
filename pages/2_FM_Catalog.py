ea5778d40e8cf1f74b1d5a58403973de54b07aab
indraPLM <112387467+indraPLM@users.noreply.github.com>
2025-03-04 13:06:53 +0700

    Update 2_FM_Catalog.py

--- pages/2_FM_Catalog.py
+++ pages/2_FM_Catalog.py
@@ -218,11 +218,11 @@ dist_lon=East-West
 if dist_lon >40:
     w=1.5
 if 30 < dist_lon < 39:
-    w=1.
+    w=1.0
 if 15 < dist_lon < 29:
-    w=0.5
+    w=0.75
 if 10 < dist_lon < 15 :
-    w=0.25
+    w=0.5
 if 5 < dist_lon < 9 :
     w=0.1
 if dist_lon < 4:
