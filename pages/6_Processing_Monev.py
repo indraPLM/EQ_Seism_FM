22fbe3f4b57d35304538b61d64648d9eac69baeb
indraPLM <112387467+indraPLM@users.noreply.github.com>
2025-03-10 11:39:31 +0700

    Update 6_Processing_Monev.py

--- pages/6_Processing_Monev.py
+++ pages/6_Processing_Monev.py
@@ -11,7 +11,7 @@ from matplotlib.pyplot import figure
 import geopandas
 import datetime
 
-st.set_page_config(page_title='Peta Focal Mechanism',  layout='wide', page_icon="🌍")
+st.set_page_config(page_title='Kecepatan Processing Gempabumi',  layout='wide', page_icon="🌍")
 
 st.sidebar.header("Input Parameter :")
 last1 = datetime.datetime.now() - datetime.timedelta(1)
