b5281b1707ab31a4523b4aa88bdbaa37c6601aac
indraPLM <112387467+indraPLM@users.noreply.github.com>
2025-03-10 10:16:22 +0700

    Update 5_Kecepatan_Monev.py

--- pages/5_Kecepatan_Monev.py
+++ pages/5_Kecepatan_Monev.py
@@ -124,9 +124,10 @@ for i in range(len(df['eventid'])):
     t_proc.append(t)
 #print(t_proc)
 df['time_proc']=t_proc
-#df_display=df[ df['mag'] >= 5]
-#df= df[(df['date_time'] > time_start) & (df['date_time'] < time_end)]
-#df= df[(df['fixedLon'] > West) & (df['fixedLon'] < East)]
-#df= df[(df['fixedLat'] > South) & (df['fixedLat'] < North)]
+df['mag']=fix_float(df['mag'])
+df_v=df[ df['mag'] >= 5]
+df_v= df_v[(df_v['date_time'] > time_start) & (df_v['date_time'] < time_end)]
+df_v= df_v[(df['lon'] > West) & (df_v['lon'] < East)]
+df_v= df_v[(df['lat'] > South) & (df_v['lat'] < North)]
 
 st.dataframe(df)
