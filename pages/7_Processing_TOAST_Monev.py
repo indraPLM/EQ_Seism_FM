a531caaa512738a89757f1f0f34a84b28ffc1fed
indraPLM <112387467+indraPLM@users.noreply.github.com>
2025-03-11 09:56:57 +0700

    Update 7_Processing_TOAST_Monev.py

--- pages/7_Processing_TOAST_Monev.py
+++ pages/7_Processing_TOAST_Monev.py
@@ -157,4 +157,9 @@ result = pd.merge(df_qc, df_toast, on="event_id")
 result['lapse_time_toast']=result['tstamp_toast']-result['date_time_wib']
 result['lapse_time_toast'] = (result['lapse_time_toast'].dt.total_seconds()/60).round(2)
 
+st.markdown(""" ### Grafik Kecepatan Prosesing TOAST M >=5 """)
+st.scatter_chart(result, x="date_time_wib", y="lapse_time_toast")
+
+st.markdown("""### Data Parameter Gempa dan Kecepatan Prosesing TOAST""")
+st.dataframe(result)
 st.dataframe(result)
