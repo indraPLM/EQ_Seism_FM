05cddf503a445dfc19b8ebebf5ba4027c71a714a
indraPLM <112387467+indraPLM@users.noreply.github.com>
2025-04-30 09:56:20 +0700

    Update 7_Processing_TOAST_Monev.py

--- pages/7_Processing_TOAST_Monev.py
+++ pages/7_Processing_TOAST_Monev.py
@@ -156,7 +156,8 @@ st.dataframe(df_toast)
 st.markdown(""" ### Test2""")
 st.dataframe(df)
 df= df[(df['date_time'] > df_toast['tstamp_toast'][0] ) & (df['date_time'] < df_toast['tstamp_toast'][len(df_toast)-1])]
-
+st.markdown(""" ### Test3""")
+st.dataframe(df)
 df_qc=df
 st.dataframe(df_qc)
 
