d29b7bdd0f7e27c492ef202306a247dd261b238c
indraPLM <112387467+indraPLM@users.noreply.github.com>
2025-04-30 09:10:34 +0700

    Update 7_Processing_TOAST_Monev.py

--- pages/7_Processing_TOAST_Monev.py
+++ pages/7_Processing_TOAST_Monev.py
@@ -162,11 +162,11 @@ result= result[(result['date_time_wib'] > time_start) & (result['date_time_wib']
 result= result[(result['lon'] > West) & (result['lon'] < East)]
 result= result[(result['lat'] > South) & (result['lat'] < North)]
 
-st.markdown(""" ### Peta Lokasi Gempabumi Prosesing TOAST M >=5 """)
-st.map(result, latitude="lat", longitude="lon", size=2000, zoom=3 )
+#st.markdown(""" ### Peta Lokasi Gempabumi Prosesing TOAST M >=5 """)
+#st.map(result, latitude="lat", longitude="lon", size=2000, zoom=3 )
 
-st.markdown(""" ### Grafik Kecepatan Prosesing TOAST M >=5 """)
-st.scatter_chart(result, x="date_time_wib", y="lapse_time_toast")
+#st.markdown(""" ### Grafik Kecepatan Prosesing TOAST M >=5 """)
+#st.scatter_chart(result, x="date_time_wib", y="lapse_time_toast")
 
 st.markdown("""### Data Parameter Gempa dan Kecepatan Prosesing TOAST""")
 st.dataframe(result)
