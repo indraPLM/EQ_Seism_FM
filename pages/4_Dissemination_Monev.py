f92c6199d082485630044e0f4efa8903708f4eb7
indraPLM <112387467+indraPLM@users.noreply.github.com>
2025-07-18 07:59:28 +0700

    Update 4_Dissemination_Monev.py

--- pages/4_Dissemination_Monev.py
+++ pages/4_Dissemination_Monev.py
@@ -11,19 +11,19 @@ st.set_page_config(page_title='TSP Monitoring dan Evaluasi', layout='wide', page
 st.sidebar.header("Input Parameter :")
 
 # Calculate dynamic dates
-yesterday = datetime.datetime.now() - datetime.timedelta(days=1)
-month_before = yesterday - datetime.timedelta(days=30)
+#yesterday = datetime.datetime.now() - datetime.timedelta(days=1)
+#month_before = yesterday - datetime.timedelta(days=30)
 
 # Format as strings
-yesterday_str = yesterday.strftime('%Y-%m-%d %H:%M:%S')
-month_before_str = month_before.strftime('%Y-%m-%d %H:%M:%S')
+#yesterday_str = yesterday.strftime('%Y-%m-%d %H:%M:%S')
+#month_before_str = month_before.strftime('%Y-%m-%d %H:%M:%S')
 
 # Sidebar inputs with default values
-time_start = st.sidebar.text_input('Start DateTime:', month_before_str)
-time_end = st.sidebar.text_input('End DateTime:', yesterday_str)
+#time_start = st.sidebar.text_input('Start DateTime:', month_before_str)
+#time_end = st.sidebar.text_input('End DateTime:', yesterday_str)
 
-#time_start = st.sidebar.text_input('Start DateTime:', '2025-03-01 00:00:00')
-#time_end   = st.sidebar.text_input('End DateTime:', '2025-03-31 23:59:59')
+time_start = st.sidebar.text_input('Start DateTime:', '2025-06-01 00:00:00')
+time_end   = st.sidebar.text_input('End DateTime:', '2025-06-30 23:59:59')
 
 # --- Helper Functions ---
 def extract_text(tag): return [t.text.strip() for t in soup.find_all(tag)]
