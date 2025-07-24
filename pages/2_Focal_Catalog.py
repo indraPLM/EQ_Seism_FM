e0c7d06f99a4019b3c8876673b3a97d15f0587fe
indraPLM <112387467+indraPLM@users.noreply.github.com>
2025-07-24 23:50:44 +0700

    Update 2_Focal_Catalog.py

--- pages/2_Focal_Catalog.py
+++ pages/2_Focal_Catalog.py
@@ -134,10 +134,10 @@ def export_to_pdf(df, filename="focal_report.pdf"):
     cols = ['DateTime','Magnitude','Type Magnitude','Latitude','Longitude','Depth',
             'Strike NP1','Dip NP1','Rake NP1','Strike NP2','Dip NP2','Rake NP2','Remark']
     col_widths = {
-        'DateTime': 30, 'Magnitude': 18, 'Type Magnitude': 20,
+        'DateTime': 30, 'Magnitude': 18, 'Type Magnitude': 22,
         'Latitude': 20, 'Longitude': 20, 'Depth': 12,
-        'Strike NP1': 12, 'Dip NP1': 12, 'Rake NP1': 10,
-        'Strike NP2': 12, 'Dip NP2': 12, 'Rake NP2': 12,
+        'Strike NP1': 15, 'Dip NP1': 12, 'Rake NP1': 10,
+        'Strike NP2': 15, 'Dip NP2': 12, 'Rake NP2': 12,
         'Remark': 45, 'Focal': 30
     }
 
