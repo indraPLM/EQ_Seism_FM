b43ee8e4604007d2d98cdd31a8c02f13064ddc30
indraPLM <112387467+indraPLM@users.noreply.github.com>
2025-07-29 17:55:47 +0700

    Update 9_Press_Release_Monev.py

--- pages/9_Press_Release_Monev.py
+++ pages/9_Press_Release_Monev.py
@@ -59,7 +59,8 @@ try:
             pdf.ln(1)
 
         buffer = BytesIO()
-        pdf.output(buffer)
+        pdf_bytes = pdf.output(dest='S').encode('latin1')  # Returns string, encode to bytes
+        buffer.write(pdf_bytes)
         buffer.seek(0)
         return buffer
 
