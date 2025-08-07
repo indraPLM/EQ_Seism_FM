59fe94f60097550dd1319d2475bd3aa6579f6340
indraPLM <112387467+indraPLM@users.noreply.github.com>
2025-08-07 15:27:24 +0700

    Update 8_EQ_Katalog-QC-PGN_Monev.py

--- pages/8_EQ_Katalog-QC-PGN_Monev.py
+++ pages/8_EQ_Katalog-QC-PGN_Monev.py
@@ -18,8 +18,8 @@ import requests
 # 🌍 Page Config
 st.set_page_config(page_title='Earthquake Dashboard - Katalog QC PGN', layout='wide', page_icon='🌋')
 st.sidebar.subheader("🕒 Select Date Range")
-start_time = st.sidebar.text_input("Start Time", "2025-07-01 00:00:00")
-end_time = st.sidebar.text_input("End Time", "2025-07-31 23:59:59")
+start_time = st.sidebar.text_input("Start Time", "2025-07-01")
+end_time = st.sidebar.text_input("End Time", "2025-07-31")
 
 # 📄 Manually specify Excel file path
 excel_path = "./pages/fileQC/Data_QC_Gempabumi_Juli_2025.xlsx"  # 🔧 Update this path as needed
