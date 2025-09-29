7f6e608dda96ce59f7491619be417d859d1a3e58
indraPLM <112387467+indraPLM@users.noreply.github.com>
2025-09-29 17:13:56 +0700

    Update 9_EQ_Katalog-Integrasi_Monev.py

--- pages/9_EQ_Katalog-Integrasi_Monev.py
+++ pages/9_EQ_Katalog-Integrasi_Monev.py
@@ -70,7 +70,7 @@ for _, row in df_filtered.iterrows():
             fill_color=depth_color(row['DEPTH']),
             fill_opacity=0.5,
             popup=folium.Popup(
-                f"<b>Date:</b> {row['DATE']}<br><b>Mag:</b> {row['MAG']}<br><b>Depth:</b> {row['DEPTH']} km",
+                f"<b>Date:</b> {row['DATETIME']}<br><b>Mag:</b> {row['MAG']}<br><b>Depth:</b> {row['DEPTH']} km",
                 max_width=250
             )
         ).add_to(m)
