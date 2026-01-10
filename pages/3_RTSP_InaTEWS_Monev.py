09e15b63434c07cf43fd8a7d18d8a202ae098d0f
indraPLM <112387467+indraPLM@users.noreply.github.com>
2026-01-10 12:43:10 +0700

    Update 3_RTSP_InaTEWS_Monev.py

--- pages/3_RTSP_InaTEWS_Monev.py
+++ pages/3_RTSP_InaTEWS_Monev.py
@@ -2,7 +2,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import requests
+import requests, datetime
 from bs4 import BeautifulSoup
 from obspy.geodetics import degrees2kilometers
 
